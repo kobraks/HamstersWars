a27624127f9e0b0b6ce3fd4b9658ac673ce94ae0
kobraks <kobraks2@gmail.com>
2018-05-27 00:25:30 +0200

    Added save option to Image class

--- HamstersWars/Image.cpp
+++ HamstersWars/Image.cpp
@@ -6,6 +6,48 @@
 #include "UnknownFormatException.h"
 #include "gl/glew.h"
 
+FREE_IMAGE_FORMAT convert(gl::util::image_type::image_type_t type)
+{
+	switch(type)
+	{
+	case gl::util::image_type::BMP:
+		return FIF_BMP;
+
+	case gl::util::image_type::EXR:
+		return FIF_EXR;
+
+	case gl::util::image_type::J2K:
+		return FIF_J2K;
+
+	case gl::util::image_type::JP2:
+		return FIF_JP2;
+
+	case gl::util::image_type::JPEG:
+		return FIF_JPEG;
+
+	case gl::util::image_type::JXR:
+		return FIF_JXR;
+
+	case gl::util::image_type::PBM:
+		return FIF_PBM;
+
+	case gl::util::image_type::PGM:
+		return FIF_PGM;
+
+	case gl::util::image_type::PNG:
+		return FIF_PNG;
+
+	case gl::util::image_type::PPM:
+		return FIF_PPM;
+
+	case gl::util::image_type::TIFF:
+		return FIF_TIFF;
+
+	default:
+		return FIF_UNKNOWN;
+	}
+}
+
 gl::util::Image::Image() : pixels_(nullptr), width_(0), height_(0)
 {
 }
@@ -94,6 +136,28 @@ void gl::util::Image::load(const std::string& file_name)
 	FreeImage_Unload(image);
 }
 
+void gl::util::Image::save(const std::string& file_name, image_type::image_type_t type)
+{
+	auto handler = FreeImage_Allocate(width_, height_, 32);
+
+	for (size_t i = 0; i < width_; ++i)
+		for (size_t j = 0; j < height_; ++j)
+		{
+			RGBQUAD color;
+
+			auto pixel = get_pixel(i, j);
+			color.rgbRed = pixel.r;
+			color.rgbGreen = pixel.g;
+			color.rgbBlue = pixel.b;
+			color.rgbReserved = pixel.a;
+
+			FreeImage_SetPixelColor(handler, i, j, &color);
+		}
+
+	FreeImage_Save(convert(type), handler, file_name.c_str(), 0);
+	FreeImage_Unload(handler);
+}
+
 unsigned gl::util::Image::width() const
 {
 	return width_;
@@ -109,7 +173,7 @@ const glm::vec4* gl::util::Image::get_pixels() const
 	return pixels_;
 }
 
-glm::vec4 gl::util::Image::get_pixels(const unsigned& x, const unsigned& y) const
+glm::vec4& gl::util::Image::get_pixel(const unsigned& x, const unsigned& y)
 {
 	if (x < width_ && height_ < y)
 		return pixels_[x + y * width_];
@@ -117,12 +181,41 @@ glm::vec4 gl::util::Image::get_pixels(const unsigned& x, const unsigned& y) cons
 		throw std::out_of_range("");
 }
 
-void gl::util::Image::set_pixels(const unsigned& x, const unsigned& y, const glm::vec4& color)
+glm::vec4 gl::util::Image::get_pixel(const unsigned& x, const unsigned& y) const
+{
+	if (x < width_ && height_ < y)
+		return pixels_[x + y * width_];
+	else
+		throw std::out_of_range("");
+}
+
+void gl::util::Image::set_pixel(const unsigned& x, const unsigned& y, const glm::vec4& color)
 {
 	if (x < width_ && height_ < y)
 		pixels_[x + y * width_] = color;
 	else
 		throw std::out_of_range("");
+}
+
+glm::vec4 gl::util::Image::operator()(const unsigned& x, const unsigned& y) const
+{
+	return get_pixel(x, y);
+}
+
+glm::vec4& gl::util::Image::operator()(const unsigned& x, const unsigned& y)
+{
+	return get_pixel(x, y);
+}
+
+glm::vec4 gl::util::Image::translate_color(const unsigned char& r, const unsigned char& g, const unsigned char& b, const unsigned char& a)
+{
+	return glm::vec4(static_cast<float>(r) / 255, static_cast<float>(g) / 255, static_cast<float>(b) / 255, static_cast<float>(a) / 255);
+}
+
+glm::vec4 gl::util::Image::translate_color(const unsigned char color[4])
+{
+	return glm::vec4(static_cast<float>(color[0]) / 255, static_cast<float>(color[1]) / 255, static_cast<float>(color[2]) / 255, static_cast<float>(color[3]) / 255);
+
 }
 
 void gl::util::Image::load_to_memory(void* image_file)
@@ -138,10 +231,7 @@ void gl::util::Image::load_to_memory(void* image_file)
 	pixels_ = new glm::vec4[width_ * height_];
 
 	for (size_t i = 0; i < width_ * height_; ++i)
-	{
-		GLubyte pixel[4] = { pixels[i * 4 + 2], pixels[i * 4 + 1], pixels[i * 4], pixels[i * 4 + 3] };
-		pixels_[i] = glm::vec4((float)pixel[0] / 255, (float)pixel[1] / 255, (float)pixel[2] / 255, (float)pixel[3] / 255);
-	}
+		pixels_[i] = translate_color(pixels[i * 4 + 2], pixels[i * 4 + 1], pixels[i * 4], pixels[i * 4 + 3]);
 
 	FreeImage_Unload(image);
 }
--- HamstersWars/Image.h
+++ HamstersWars/Image.h
@@ -4,6 +4,24 @@
 
 namespace gl::util
 {
+	namespace image_type
+	{
+		enum image_type_t
+		{
+			BMP,
+			EXR,
+			J2K,
+			JP2,
+			JPEG,
+			JXR,
+			PNG,
+			PBM,
+			PGM,
+			PPM,
+			TIFF
+		};
+	}
+
 	class Image
 	{
 	public:
@@ -21,13 +39,22 @@ namespace gl::util
 		void load(unsigned char* pixels, const unsigned int& size);
 		void load(const std::string& file_name);
 
+		void save(const std::string& file_name, image_type::image_type_t type);
+
 		unsigned int width() const;
 		unsigned int height() const;
 
 		const glm::vec4* get_pixels() const;
 
-		glm::vec4 get_pixels(const unsigned int& x, const unsigned int& y) const;
-		void set_pixels(const unsigned int& x, const unsigned int& y, const glm::vec4& color);
+		glm::vec4& get_pixel(const unsigned int& x, const unsigned int& y);
+		glm::vec4 get_pixel(const unsigned int& x, const unsigned int& y) const;
+		void set_pixel(const unsigned int& x, const unsigned int& y, const glm::vec4& color);
+
+		glm::vec4 operator()(const unsigned int& x, const unsigned int& y) const;
+		glm::vec4& operator()(const unsigned int& x, const unsigned int& y);
+
+		static glm::vec4 translate_color(const unsigned char& r, const unsigned char& g, const unsigned char& b, const unsigned char& a);
+		static glm::vec4 translate_color(const unsigned char color[4]);
 	private:
 		glm::vec4* pixels_;
 		unsigned int width_;
